92115eb57669a1d3aebe0cf0e1cb62ed3c27848c
12862 <daniel.gosses@vtsd.com>
2024-12-09 09:24:22 -0500

    JavaScript Test

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,18 @@
 /* Put code for the task below */
+<h3>Task 1 - The Image</h3>
+<img onClick="functionName()" src="assets/img/compIcon.png" id="imageTask1"/>
+<br/>
+<button type="button" onClick="toggle()">Toggle</button>
+<script>
+    function toggle() {
+
+    const image = document.getElementById("imageTask1");
+
+
+    if (image.style.display === "block"){
+    image.style.display = "none";
+} else {
+    image.style.display = "block";
+}
+}
+</script>
