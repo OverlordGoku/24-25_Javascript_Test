8cd8ec643c702804549de06fe957f7dea8d4675c
12862 <daniel.gosses@vtsd.com>
2024-12-10 08:41:38 -0500

    JavaScript Test

--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -1,1 +1,8 @@
 /* Put code for the task below */
+<button onClick="displayMessage()">Click me</button>
+<script>
+    function displayMessage() {
+    alert("I clicked it!");
+}
+</script>
+
--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,1 +1,20 @@
 /* Put code for the task below */
+<script>
+    function add(){
+    let value1 = Number(document.getElementById("value1").value);
+    let value2 = Number(document.getElementById("value2").value);
+
+    alert("Your total is:" + (value1 + value2));
+}
+    function check() {
+    let value1 = document.getElementById("value1").value;
+    let value2 = document.getElementById("value2").value;
+
+    if (value1 == "") {
+    document.getElementById("value1").style.backgroundColor = "red";
+}
+    if (value2 == "") {
+    document.getElementById("value2").style.backgroundColor = "red";
+}
+}
+</script>
