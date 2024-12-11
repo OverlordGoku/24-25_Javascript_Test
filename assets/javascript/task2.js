67cdd7936459cde2f186e2b7c33cd0d88620a342
OverlordGoku <118377088+OverlordGoku@users.noreply.github.com>
2024-12-11 00:05:44 -0500

    Update task2.js

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,6 @@
 /* Put code for the task below */
+<script>
+  document.getElementById('redButton').addEventListener('click', function() { document.getElementById('colorTable').style.backgroundColor = 'red'; }); document.getElementById('greenButton').addEventListener('click', function() { document.getElementById('colorTable').style.backgroundColor = 'green'; }); 
+document.getElementById('blueButton').addEventListener('click', function() { 
+  document.getElementById('colorTable').style.backgroundColor = 'blue';
+  <script/>
