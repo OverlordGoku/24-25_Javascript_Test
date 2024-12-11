745815c9a3b81e6a1aecb6e81a61eb5283277ca8
OverlordGoku <118377088+OverlordGoku@users.noreply.github.com>
2024-12-11 00:02:51 -0500

    Update task4.js

--- assets/javascript/task4.js
+++ assets/javascript/task4.js
@@ -1,1 +1,6 @@
 /* Put code for the task below */
+<script>
+document.getElementById('clearButton').addEventListener('click', function() { 
+  // Create a new div element var newPage = document.createElement('div'); newPage.textContent = "New page has been created"; 
+  // Clear the existing content and append the new page var contentDiv = document.getElementById('content'); contentDiv.innerHTML = ''; contentDiv.appendChild(newPage);
+                                                        <script/>
